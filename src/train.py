9dc0f972a56ffb3fd4da2c02036523d29381d3e8
TheMOHD3886 <65400638+TheMOHD3886@users.noreply.github.com>
2026-04-08 13:25:39 +0200

    feat: added spaced cipher support
    - argparse: added an argument for training the model with spaced data

--- src/train.py
+++ src/train.py
@@ -1,10 +1,24 @@
+import argparse
 from classes.trainer import JambaTrainingPipeline
 from classes.config import Config
 
 
 def main() -> None:
     """Execute the training loop, handling checkpoints and final model saving."""
+    parser = argparse.ArgumentParser(description="Train Jamba Cipher Model")
+
+    parser.add_argument(
+        "--spaced",
+        action="store_true",
+        help="Flag to train on the spaced dataset instead of the normal dataset.",
+    )
+
+    args = parser.parse_args()
+
     cfg = Config()
+
+    cfg.use_spaces = args.spaced
+
     pipeline = JambaTrainingPipeline(config=cfg)
     pipeline.run()
 
