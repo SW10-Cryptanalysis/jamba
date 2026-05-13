fc12c22d0846caa9d917cdbaaa2388696675ecfc
TheMOHD3886 <65400638+TheMOHD3886@users.noreply.github.com>
2026-05-13 11:51:40 +0200

    feat: update config for ucloud training

--- src/classes/config.py
+++ src/classes/config.py
@@ -134,7 +134,7 @@ class Config:
 
     jamba_config: JambaConfig = field(default_factory=JambaConfig)
 
-    batch_size: int = 2
+    batch_size: int = 16
     grad_accum: int = 8
     learning_rate: float = 3e-4
     epochs: int = 3
@@ -144,8 +144,8 @@ class Config:
     dataloader_num_workers: int = 4
 
     log_steps: int = 50
-    save_steps: int = 1000
-    eval_steps: int = 1000
+    save_steps: int = 25000
+    eval_steps: int = 10000
     use_spaces: bool = False
 
     data_dir: Path = BASE_DIR / "Ciphers"
