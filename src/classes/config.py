d06d58774ed084dd4006abb140cba9b9ef44141f
TheMOHD3886 <65400638+TheMOHD3886@users.noreply.github.com>
2026-05-21 11:00:58 +0200

    fix: test config on ucloud

--- src/classes/config.py
+++ src/classes/config.py
@@ -148,8 +148,8 @@ class Config:
     dataloader_num_workers: int = 4
 
     log_steps: int = 50
-    save_steps: int = 2000
-    eval_steps: int = 1000
+    save_steps: int = 100
+    eval_steps: int = 50
     use_spaces: bool = False
 
     data_dir: Path = BASE_DIR / "Ciphers"
